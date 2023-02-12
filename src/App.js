5447f49488da53a2309ae30f910d781be87d89d1
ellinacv <113457842+ellinacv@users.noreply.github.com>
2023-02-12 23:01:32 +0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export const App = () => {
+  return (
+    <div className="todo">
+      <div className="todo-sidebar"></div>
+      <div className="todo-tasks"></div>
+    </div>
+  );
+};
